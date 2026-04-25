bcfbc19ed8be653b99a2b06230005a0fcf80dc06
Rohan Das <mailtorohan.97@gmail.com>
2026-04-25 17:48:52 +0530

    retrieval pipeline added

---
+++ core/retrieval.py
@@ -0,0 +1,12 @@
+from utils.utils import timed
+
+class RetrievalPipeline:
+
+    def __init__(self, vectorstore, k=3):
+        self.retriever = vectorstore.as_retriever(
+            search_kwargs={"k": k}
+        )
+
+    @timed
+    def retrieve(self, query):
+        return self.retriever.get_relevant_documents(query)
